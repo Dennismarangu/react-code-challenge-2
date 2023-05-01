dd3a2280602b6b7c007ff0eb64265ff8fe4869cc
Dennismarangu <dennismutumamarangu@gmail.com>
2023-05-01 22:45:13 +0300

    New commit

--- bot-battlr-app/src/App.js
+++ bot-battlr-app/src/App.js
@@ -9,7 +9,7 @@ function App() {
   useEffect(() => {
     fetch("http://localhost:3000/bots")
       .then((response) => response.json())
-      .then((data) => setBots(data.bots))
+      .then((data) => setBots(data))
       .catch((error) => console.error(error));
       console.log(bots);
   }, [bots]);
